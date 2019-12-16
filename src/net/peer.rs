6ab6a069f1a8fcc64b71366a66cba913da5666ab
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2019-12-16 22:05:17 +0000

    Peer handler

--- src/net/peer.rs
+++ src/net/peer.rs
@@ -1,2 +1,18 @@
+use bitcoin::network::message::NetworkMessage;
+use failure::Error;
+
 /// Unique ID for a connected peer
 pub type PeerId = usize;
+
+/// Trait that handles peer events
+pub trait PeerListener {
+    fn handle_open(&self, peer: &Peer) {}
+    fn handle_close(&self, peer: &Peer, connected: bool) {}
+    fn handle_packet(&self, peer: &Peer, packet: &NetworkMessage) {}
+    fn handle_error(&self, peer: &Peer, error: &Error) {}
+    fn handle_ban(&self, peer: &Peer) {}
+    fn handle_connect(&self, peer: &Peer) {}
+}
+
+/// A remote node to send and receive P2P network messages
+pub struct Peer {}
