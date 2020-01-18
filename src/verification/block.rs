f306a99fce43184ccd539332b2f5d3f27b28cacb
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2020-01-18 20:23:43 +0000

    More block verification: coinbase height and weight

--- src/verification/block.rs
+++ src/verification/block.rs
@@ -1,12 +1,17 @@
 use super::TransactionVerifier;
+use crate::protocol::consensus::*;
 use crate::util::EmptyResult;
-use bitcoin::Block;
+use bitcoin::{
+    blockdata::constants::*, blockdata::script::Builder, consensus::encode::VarInt, Block,
+};
 use failure::{bail, ensure, Error};
 
 pub trait BlockVerifier {
     fn get_claimed(&self) -> Result<u64, Error>;
     fn validate_pow(&self) -> EmptyResult;
     fn check_body(&self) -> EmptyResult;
+    fn get_weight(&self) -> usize;
+    fn check_coinbase_height(&self, height: u32) -> EmptyResult;
 }
 
 impl BlockVerifier for Block {
@@ -25,6 +30,70 @@ impl BlockVerifier for Block {
     }
 
     fn check_body(&self) -> EmptyResult {
-        todo!("non contextual block verification");
+        // Check the merkle root.
+        ensure!(self.check_merkle_root(), "bad-txnmrklroot");
+
+        // Size limits
+
+        ensure!(!self.txdata.is_empty(), "bad-blk-length");
+        ensure!(
+            self.txdata.len() * WITNESS_SCALE_FACTOR <= MAX_BLOCK_WEIGHT as usize,
+            "bad-blk-length"
+        );
+        // TODO: size without witness * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT as usize
+
+        // First transaction must be coinbase
+        ensure!(self.txdata[0].is_coin_base(), "bad-cb-missing");
+
+        // Check transactions
+
+        let mut sigops = 0;
+        for (index, tx) in self.txdata.iter().enumerate() {
+            if index > 0 && tx.is_coin_base() {
+                bail!("bad-cb-multiple")
+            }
+
+            tx.check_sanity()?;
+
+            sigops += tx.get_legacy_sig_op_count();
+        }
+
+        ensure!(
+            sigops * WITNESS_SCALE_FACTOR <= MAX_BLOCK_SIGOPS_COST,
+            "bad-blk-sigops"
+        );
+
+        Ok(())
+    }
+
+    fn get_weight(&self) -> usize {
+        let header = 80;
+        let tx_len = VarInt(self.txdata.len() as u64).len();
+        let tx = self
+            .txdata
+            .iter()
+            .fold(0, |total, tx| total + tx.get_weight());
+
+        header + tx_len + tx
+    }
+
+    fn check_coinbase_height(&self, height: u32) -> EmptyResult {
+        ensure!(self.header.version >= 2);
+
+        ensure!(!self.txdata.is_empty());
+
+        let coinbase = &self.txdata[0];
+
+        ensure!(!coinbase.input.is_empty());
+
+        let coinbase_script = &coinbase.input[0].script_sig;
+
+        let expected = Builder::new().push_scriptint(height as i64).into_script();
+
+        if expected[..] == coinbase_script[0..expected.len()] {
+            Ok(())
+        } else {
+            bail!("bad-cb-height")
+        }
     }
 }
