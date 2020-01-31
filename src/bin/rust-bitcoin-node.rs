1691afa0b98430b05d119817030816cd4c4776f5
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2020-01-31 23:58:01 +0000

    main bin does IBD, update dependencies

--- src/bin/rust-bitcoin-node.rs
+++ src/bin/rust-bitcoin-node.rs
@@ -1,13 +1,38 @@
 use log::LevelFilter;
-use rust_bitcoin_node::{net::P2P, protocol::NetworkParams};
+use rust_bitcoin_node::{
+    blockchain::{Chain, ChainOptions},
+    net::P2P,
+    protocol::{NetworkParams},
+};
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::sync::Arc;
+
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::builder()
-        .filter_module("rust_bitcoin_node", LevelFilter::Trace)
+        .filter_module("rust_bitcoin_node", LevelFilter::Debug)
         .format_timestamp_millis()
         .init();
-    P2P::new(NetworkParams::default(), vec![], 8);
+
+    let mut chain = Chain::new(ChainOptions {
+        network: NetworkParams::from_network(bitcoin::Network::Bitcoin),
+        path: PathBuf::from_str("./data").unwrap(),
+    });
+    chain.open().await.unwrap();
+
+    let chain = Arc::new(Mutex::new(chain));
+
+    P2P::new(
+        chain,
+        NetworkParams::from_network(bitcoin::Network::Bitcoin),
+        vec![],
+        8,
+    )
+    .await;
+
     tokio::signal::ctrl_c().await?;
 
     Ok(())
